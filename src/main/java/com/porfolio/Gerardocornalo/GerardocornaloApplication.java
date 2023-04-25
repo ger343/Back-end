a4b3cc93a421c3659d481518c0e24ad1c08d51d8
Gerardo Cornalo <gerardocornalo@gmail.com>
2023-04-25 17:17:58 -0300

    Primer Commit

---
+++ src/main/java/com/porfolio/Gerardocornalo/GerardocornaloApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.Gerardocornalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerardocornaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerardocornaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porfolio/GustavoP/GustavoPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.GustavoP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustavoPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
