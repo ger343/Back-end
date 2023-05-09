e9f160f96c1395a92236cd9340485fa4e959c478
Gerardo Cornalo <gerardocornalo@gmail.com>
2023-05-09 20:48:30 -0300

    se agregaron las politicas de CORS

--- src/main/java/com/porfolio/Gerardocornalo/Controller/PersonaController.java
+++ src/main/java/com/porfolio/Gerardocornalo/Controller/PersonaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-gerardo.web.app"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
